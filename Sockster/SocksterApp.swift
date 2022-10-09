12a6dc9a5f764cb4cd8ea6d88115853d405a8f59
Alek Sai <lenyapugachev@me.com>
2022-10-09 14:35:25 +0300

    Initial Commit

---
+++ Sockster/SocksterApp.swift
@@ -0,0 +1,20 @@
+//
+//  SocksterApp.swift
+//  Sockster
+//
+//  Created by Alek Sai on 9.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SocksterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
