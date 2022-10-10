3fcad2759bf3330bcabe7ea19ba2416fc190c906
Alek Sai <lenyapugachev@me.com>
2022-10-10 19:39:58 +0300

    Exit on close

--- Sockster/SocksterApp.swift
+++ Sockster/SocksterApp.swift
@@ -9,12 +9,51 @@ import SwiftUI
 
 @main
 struct SocksterApp: App {
+    @State var gvm = GlobalViewModel()
+    
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }.windowStyle(HiddenTitleBarWindowStyle())
+            ZStack {
+                HostingWindowFinder { window in
+                    if let window = window {
+                        gvm.addWindow(window: window)
+                    }
+                }
+                ContentView()
+                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            }
+        }
+        .windowStyle(HiddenTitleBarWindowStyle())
+        .handlesExternalEvents(matching: Set(arrayLiteral: "*"))
     }
+}
+
+class GlobalViewModel : NSObject, ObservableObject {
+    @Published var windows = Set<NSWindow>()
+    
+    func addWindow(window: NSWindow) {
+        window.delegate = self
+        windows.insert(window)
+    }
+}
+
+extension GlobalViewModel : NSWindowDelegate {
+    func windowWillClose(_ notification: Notification) {
+        NSApplication.shared.terminate(self)
+    }
+}
+
+struct HostingWindowFinder: NSViewRepresentable {
+    var callback: (NSWindow?) -> ()
+
+    func makeNSView(context: Self.Context) -> NSView {
+        let view = NSView()
+        DispatchQueue.main.async { [weak view] in
+            self.callback(view?.window)
+        }
+        return view
+    }
+    func updateNSView(_ nsView: NSView, context: Context) {}
 }
